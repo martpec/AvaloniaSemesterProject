6583ae3bfb45c63c41574cffe8146d347b351bfb
JakubPotocky <jakubpatoky@gmaail.com>
2024-05-04 20:04:15 +0200

    implemented ui design

--- OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
+++ OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ac2fe79d5536f4af5d21896f0a613d2bf14439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2823631f1011ffc732e63f6aeca54d652594ed13")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

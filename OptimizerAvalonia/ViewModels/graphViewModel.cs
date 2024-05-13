141ff1601910f7f2005203ccc1de7dd5684c3424
JakubPotocky <jakubpatoky@gmaail.com>
2024-05-13 19:19:46 +0200

    1 graph display

--- OptimizerAvalonia/ViewModels/graphViewModel.cs
+++ OptimizerAvalonia/ViewModels/graphViewModel.cs
@@ -1,9 +1,54 @@
 using System.Reactive;
 using ReactiveUI;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 namespace OptimizerAvalonia.ViewModels;
 
 public class graphViewModel: ViewModelBase
 {
+    private readonly ObservableCollection<ObservablePoint> _observablePoints;
+    public ObservableCollection<ISeries> Series { get; set; }
+    public graphViewModel()
+    {
+        _observablePoints = new ObservableCollection<ObservablePoint>
+        {
+            new ObservablePoint(1,0),
+            new(2,1.2),
+            new(3,2.2),
+            new(4,3.2),
+            new(5,4.2),
+            new(6,5.2),
+            new(7,6.2),
+            new(8,7.2),
+            new(9,8.2),
+            new(10,9.2),
+            new(11,10.2),
+            new(12,11.2),
+            new(13,50.2)
+        };
 
+        Series = new ObservableCollection<ISeries>
+        {
+            new LineSeries<ObservablePoint>
+            {
+                Values = _observablePoints,
+                Fill = null
+            }
+        };
+    }
+    private int x = 13;
+    public void AddItemCommand()
+    {
+        _observablePoints.Add(new(++x,1));
+    }   
 }
--- OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
+++ OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a7f62ed7bf009d843f82eb82acd3a8a6d01861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c1e0c19521dab7a381054aa9af27fbc0e3f306")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

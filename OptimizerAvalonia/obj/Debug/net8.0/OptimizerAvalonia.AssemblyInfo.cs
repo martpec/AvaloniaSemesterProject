101a1e8564dffaf80fd8cf943a95324579c19fd5
FilipFekete <fifek23@student.sdu.dk>
2024-05-14 00:44:21 +0200

    Initial commit

--- OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
+++ OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a7f62ed7bf009d843f82eb82acd3a8a6d01861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2c20db95c553408b0ecf4b1f6fb2e2959b045e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

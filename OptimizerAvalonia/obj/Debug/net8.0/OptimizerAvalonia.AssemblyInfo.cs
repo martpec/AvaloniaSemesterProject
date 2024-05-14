82f5c963c4b9b45f0f36ae83652e0a788fb72a2d
JakubPotocky <jakubpatoky@gmaail.com>
2024-05-14 09:42:27 +0200

    DateTime main graph fix

--- OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
+++ OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141ff1601910f7f2005203ccc1de7dd5684c3424")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52987c840819efa7f6bdab632ebab8b2efb5b13f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

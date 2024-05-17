387ad38a6f7494fde03a4c0f62bd1c088bbccba2
JakubPotocky <jakubpatoky@gmaail.com>
2024-05-17 16:09:15 +0200

    graphs +legend

--- OptimizerAvalonia/ViewModels/graphViewModel.cs
+++ OptimizerAvalonia/ViewModels/graphViewModel.cs
@@ -6,7 +6,6 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.SkiaSharpView.Painting;
 using SkiaSharp;
-
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -17,7 +16,11 @@ namespace OptimizerAvalonia.ViewModels;
 
 public partial class graphViewModel : ViewModelBase
 {
-    private readonly ObservableCollection<DateTimePoint> _observablePoints;
+    private readonly ObservableCollection<DateTimePoint> allBoilers;
+    private readonly ObservableCollection<DateTimePoint> boiler1;
+    private readonly ObservableCollection<DateTimePoint> boiler2;
+    private readonly ObservableCollection<DateTimePoint> boiler3;
+    private readonly ObservableCollection<DateTimePoint> boiler4;
     public ObservableCollection<ISeries> Series { get; set; }
     public Axis[] XAxes { get; set; } =
     {
@@ -35,9 +38,16 @@ public partial class graphViewModel : ViewModelBase
                 LabelsPaint = new SolidColorPaint(SKColors.White)
             }
     };
+
+    public SolidColorPaint LegendColor { get; set; } = 
+        new SolidColorPaint 
+        { 
+            Color = new SKColor(255,255,255), 
+            //SKTypeface = SKTypeface.FromFamilyName("Courier New") 
+        }; 
     public graphViewModel()
     {
-        _observablePoints = new ObservableCollection<DateTimePoint>
+        allBoilers = new ObservableCollection<DateTimePoint>
         {   // year, month, day, hour, minute, second
             new DateTimePoint(new DateTime(2021, 1, 1, 1, 0, 0), 3.3),
             new DateTimePoint(new DateTime(2021, 1, 1, 2, 0, 0), 13.4),
@@ -48,20 +58,67 @@ public partial class graphViewModel : ViewModelBase
             new DateTimePoint(new DateTime(2021, 1, 1, 9, 0, 0), 15.3),
             new DateTimePoint(new DateTime(2021, 1, 1, 10, 0, 0), 9.2),
         };
-
+        boiler1 = new ObservableCollection<DateTimePoint>
+        {
+            new DateTimePoint(new DateTime(2021, 1, 1, 1, 0, 0), 4.1),
+            new DateTimePoint(new DateTime(2021, 1, 1, 2, 0, 0), 8.1),
+            new DateTimePoint(new DateTime(2021, 1, 1, 3, 0, 0), 2.2),
+            new DateTimePoint(new DateTime(2021, 1, 1, 4, 0, 0), 8.2),
+            new DateTimePoint(new DateTime(2021, 1, 1, 6, 0, 0), 9.2),
+            new DateTimePoint(new DateTime(2021, 1, 1, 8, 0, 0), 5.2),
+            new DateTimePoint(new DateTime(2021, 1, 1, 9, 0, 0), 1.2)
+        };
+        boiler2 = new ObservableCollection<DateTimePoint>
+        {
+            new DateTimePoint(new DateTime(2021, 1, 1, 3, 0, 0), 2.2),
+            new DateTimePoint(new DateTime(2021, 1, 1, 4, 0, 0), 2.2),
+            new DateTimePoint(new DateTime(2021, 1, 1, 9, 0, 0), 1.2)
+        };
+        boiler3 = new ObservableCollection<DateTimePoint>
+        {
+            new DateTimePoint(new DateTime(2021, 1, 1, 9, 0, 0), 1.2)
+        };
+        boiler4 = new ObservableCollection<DateTimePoint>
+        {
+            new DateTimePoint(new DateTime(2021, 1, 1, 9, 0, 0), 1.2)
+        };
         Series = new ObservableCollection<ISeries>
         {
+            
+            new StackedColumnSeries<DateTimePoint>
+            {
+                Name = "Boiler 1",
+                Values = boiler1,
+                //Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0)) // Red color
+                //Color = new SKColor(50, 50, 50),
+            },
+            new StackedColumnSeries<DateTimePoint>
+            {
+                Name = "Boiler 2",
+                Values = boiler2
+            },
+            new StackedColumnSeries<DateTimePoint>
+            {
+                Name = "Boiler 3",
+                Values = boiler3
+            },
+            new StackedColumnSeries<DateTimePoint>
+            {
+                Name = "Boiler 4",
+                Values = boiler4
+            },
             new LineSeries<DateTimePoint>
             {
-                Values = _observablePoints,
-                Fill = null
-            }
+                Name = "Total heat producet",
+                Values = allBoilers,
+                //Fill = null
+            },
         };
     }
     /*private int x = 13;
     public void AddItemCommand()
     {
-        _observablePoints.Add(new(++x,1));
+        allBoilers.Add(new(++x,1));
     } */
 
     [RelayCommand]
@@ -69,7 +126,7 @@ public partial class graphViewModel : ViewModelBase
     {
         SourceDataManager SDM = new SourceDataManager();
         // TO DO: change IBoilersList
-        Optimizer O = new Optimizer(IBoilersList,SDM.LoadSourceData(SourceDataPath));
+        Optimizer O = new Optimizer(IBoilersList, SDM.LoadSourceData(SourceDataPath));
         O.CalculateOptimalHeatProduction();
     }
 }
--- OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
+++ OptimizerAvalonia/obj/Debug/net8.0/OptimizerAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aacc2315d79ce13b16532ef281c32c79c79a891")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4296a5fd7453558a58a1b0f2fad0a01cbb78c14")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptimizerAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
